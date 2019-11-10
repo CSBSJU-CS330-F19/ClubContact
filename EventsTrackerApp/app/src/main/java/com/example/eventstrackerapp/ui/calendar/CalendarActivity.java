881d474c7baddeee7ebedf87524fb096190db7c8
Sara Laufers <slaufers001@csbsju.edu>
2019-11-10 14:14:23 -0600

    Finished Calendar GUI but not Events
    This commit holds a finished looking GUI for the calendar including all years and months. However, I have not yet implemented the events into the calendar yet.

---
+++ EventsTrackerApp/app/src/main/java/com/example/eventstrackerapp/ui/calendar/CalendarActivity.java
@@ -0,0 +1,27 @@
+package com.example.eventstrackerapp.ui.calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.GridView;
+
+import com.example.eventstrackerapp.Event;
+import com.example.eventstrackerapp.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class CalendarActivity extends AppCompatActivity {
+
+    private static final String TAG = "CalendarActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        CustomView mView = findViewById(R.id.custom_calendar);
+    }
+}
