1e159b53b3850bb0d0f62fc1bc5eb9e5b3569f97
Ian Bush <36679929+IBUSH001@users.noreply.github.com>
2019-11-10 16:30:25 -0600

    setting up RecyclerView functioning

--- EventsTrackerApp/app/src/main/java/com/example/eventstrackerapp/profile/Admin.java
+++ EventsTrackerApp/app/src/main/java/com/example/eventstrackerapp/profile/Admin.java
@@ -1,9 +1,59 @@
 package com.example.eventstrackerapp.profile;
+import com.example.eventstrackerapp.Event;
+import com.google.firebase.auth.FirebaseAuth;
 
-public class Admin extends User {
+public class Admin {
 
-    public Admin(String username)
+    private String username, password, email;
+
+    public Admin(String u, String p, String e)
     {
-        super(username,"admin");
+        username = u;
+        password = p;
+        email = e;
+    }
+
+    public String getUsername()
+    {
+        return this.username;
+    }
+
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setUsername(String u)
+    {
+        this.username = u;
+    }
+
+    public void setPassword(String p)
+    {
+        this.password = p;
+    }
+
+    public void setEmail(String e)
+    {
+        this.email = e;
+    }
+
+    public void deleteUser(User u)
+    {
+
+    }
+
+    public void deleteEvent(Event e)
+    {
+
+    }
+
+    public void deleteClub(Club c)
+    {
+
     }
 }
--- EventsTrackerApp/app/src/main/java/com/example/eventstrackerapp/profile/Club.java
+++ EventsTrackerApp/app/src/main/java/com/example/eventstrackerapp/profile/Club.java
@@ -1,12 +1,17 @@
 package com.example.eventstrackerapp.profile;
 import java.util.ArrayList;
+import com.example.eventstrackerapp.Event;
 
+/**
+ * Holds information for a specific club, including their name,
+ */
 public class Club {
     private String name;
     private String description;
+    private String email;
     private int numMembers, numEvents;
     private ArrayList<User> members;
-    //private ArrayList<Event> events;
+    private ArrayList<Event> events;
     private ArrayList<ClubExec> executives;
 
     public Club(String name)
@@ -32,5 +37,30 @@ public class Club {
     public int getNumEvents()
     {
         return this.numEvents;
+    }
+
+    public ArrayList<User> getMembers()
+    {
+        return members;
+    }
+
+    public ArrayList<Event> getEvents()
+    {
+        return events;
+    }
+
+    public ArrayList<ClubExec> getExecutives()
+    {
+        return executives;
+    }
+
+    public void addEvent(Event e)
+    {
+        events.add(e);
+    }
+
+    public void removeEvent(Event e)
+    {
+        events.remove(e);
     }
 }
